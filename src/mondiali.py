60e8bb9dc86f7f9c1cbc3a8e42f1c32b784e0f82
m3rlinux <m3rlinux.it@gmail.com>
2022-12-01 19:04:37 +0100

    aggiunta licenza Apache 2.0

--- src/mondiali.py
+++ src/mondiali.py
@@ -2,16 +2,23 @@
 # -*- coding: utf-8 -*- 
 #----------------------------------------------------------------------------
 # version     : 0.2.0
-# Created By  : Davide Gibilisco
-# e-mail      : m3rlinux.it@gmail.com
 # Created Date: 30/11/2022
+# Copyright 2022 Davide Gibilisco <m3rlinux.it@gmail.com>
 # ---------------------------------------------------------------------------
 
-""" 
-Programmino che mostra i risultati delle partite 
-del giorno corrente del mondialie Qatar 2022
+"""
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-"""  
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 # ---------------------------------------------------------------------------
 # Imports
